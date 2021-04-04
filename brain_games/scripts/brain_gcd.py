356d6ce4cde7bee802195836ef8e963259327e5d
Mqtaw <mqtaw@yandex.ru>
2021-04-04 13:02:45 +0300

    Change name. GSD >> GCD

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from brain_games.engine import begin_game
-from brain_games.games import gsd
+from brain_games.games import gcd
 
 
 def main():
-    begin_game(gsd)
+    begin_game(gcd)
 
 
 if __name__ == '__main__':
